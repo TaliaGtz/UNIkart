d578c50d6a08ad17b3ba492c2e3d08c939bed9eb
TaliaGtz <98000032+TaliaGtz@users.noreply.github.com>
2022-11-28 20:53:43 -0600

    similares

--- Producto/Producto.php
+++ Producto/Producto.php
@@ -155,27 +155,35 @@ include("../PhpDocs/PhpInclude.php");
         <div class="prodSimi">
             <h3>Productos similares</h3>
             <div class="prodSimi">
-                <div class="card">
-                    <img src="../ExtraDocs/Soup.png" width="150px" height="150px">
-                    <h4>Producto</h4>
-                    <p>Precio/Cotización</p>
-                    <br>
-                    <a href="../Producto/Producto.php">Leer más</a>
-                </div>
-                <div class="card">
-                    <img src="../ExtraDocs/Soup.png" width="150px" height="150px">
-                    <h4>Producto</h4>
-                    <p>Precio/Cotización</p>
-                    <br>
-                    <a href="../Producto/Producto.php">Leer más</a>
-                </div>
-                <div class="card">
-                    <img src="../ExtraDocs/Soup.png" width="150px" height="150px">
-                    <h4>Producto</h4>
-                    <p>Precio/Cotización</p>
-                    <br>
-                    <a href="../Producto/Producto.php">Leer más</a>
-                </div>
+                <?php
+
+                    include("../PhpDocs/Conexion.php");
+
+                    $consulta = "SELECT ID_Producto, Nombre, Precio, PrecioCant  FROM productos";
+                    $ejecutar = $conexion->query($consulta);
+                    $j = 2;
+                    for($i = 0; $i < $j; $i++){
+                        $fila = $ejecutar->fetch_array();
+
+                        if($_SESSION['Producto'] == $fila['Nombre']){
+                            $j++;continue;
+                        }
+                ?>
+
+                    <div class="card"><a href="../Producto/Producto.php?IDBtn=<?php echo $fila['ID_Producto']; ?>">
+                        <img src="../ExtraDocs/Soup.png" width="150px" height="150px">
+                        <h4><?php echo $fila['Nombre'] ?></h4>
+                        <?php
+                            if($fila['Precio'] == 0){
+                                ?><p><a id="cotiz" href="../Chat/chat.php">Cotización</a></p><?php
+                            }else{
+                                ?><p>$<?php echo $fila['PrecioCant']; ?></p><?php
+                            }
+                        ?>
+                        <br>
+                    </a></div>
+
+                <?php } ?> 
             </div>
         </div>
 
