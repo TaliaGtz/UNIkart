e9479fedb59e752d99bcb4a1db4add43967d8db8
TaliaGtz <98000032+TaliaGtz@users.noreply.github.com>
2022-12-10 23:07:58 -0600

    categorías plus

--- Category/Categories.php
+++ Category/Categories.php
@@ -26,3 +26,30 @@
     </a>
 
 <?php endwhile; ?>  
+
+<?php
+
+    //Queremos el ID de la categoría accedida
+    $IDCat      = $_SESSION['IDCategory'];
+        
+    $consultaCatNeg  =  "SELECT C.ID_Categoria, CN.ID_Producto, N.Nombre
+                        FROM categorias C
+                        INNER JOIN productoxcat CN ON C.ID_Categoria = CN.ID_Categoria
+                        INNER JOIN productos N ON CN.ID_Producto = N.ID_Producto
+                        WHERE C.ID_Categoria = '$IDCat'";
+    $ejecutar = $conexion->query($consultaCatNeg);
+
+    while($fila = $ejecutar->fetch_array()):
+        
+?>
+
+    <a href="../Producto/Producto.php?IDBtn=<?php echo $fila['ID_Producto']; ?>" class="card">
+        <div>
+            <div>
+                <img class="text" src="../ExtraDocs/OrangeBlack.png" width="150px" height="150px">
+                <h3 class="text"><?php echo $fila['Nombre']; ?></h3>
+            </div>
+        </div>
+    </a>
+
+<?php endwhile; ?> 
