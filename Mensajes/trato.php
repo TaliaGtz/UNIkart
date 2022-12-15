c9f50e17b9b5f52475dd0cb4acd902d52741da88
TaliaGtz <98000032+TaliaGtz@users.noreply.github.com>
2022-12-15 13:45:22 -0600

    sp_upTrato codigo limpio

--- Mensajes/trato.php
+++ Mensajes/trato.php
@@ -9,15 +9,10 @@
         echo $trato;    //UPDATE PRECIOCANT IN PRODUCTOS
         $query = 'CALL sp_upTrato(1, "'.$trato.'", "'.$IDProd.'", "", "");';
 
-        /*$query = "UPDATE productos
-                SET PrecioCant = '$trato'
-                WHERE ID_Producto = '$IDProd'";*/
-
         mysqli_query($conexion, $query);
 
         $url = "Mensajes/mensajes.php?IDProd=$IDProd";
         include("../PhpDocs/header.php");
-        //header("Location: http://localhost:8080/unikart2/Mensajes/mensajes.php?IDProd=$IDProd");
     }
 
 
@@ -28,16 +23,11 @@
         $tratoLugar = $_POST["tratoLugar"];
 
         $query = 'CALL sp_upTrato(2, "", "", "'.$tratoLugar.'", "'.$CODE.'");';
-        
-        /*$query = "UPDATE entregas
-                SET Lugar = '$tratoLugar'
-                WHERE CODE = '$CODE'";*/
 
         mysqli_query($conexion, $query);
 
         $url = "Mensajes/mensajes.php?Rol=$ID&COD=$CODE&IDEnt=$IDEnt";
         include("../PhpDocs/header.php");
-        //header("Location: http://localhost:8080/unikart2/Mensajes/mensajes.php?Rol=$ID&COD=$CODE");
     } 
 
 ?>
